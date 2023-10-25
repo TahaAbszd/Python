fe8a9ce9f616485f79a05c08cc552370010184e0
TahaAbszd <146336406+TahaAbszd@users.noreply.github.com>
2023-10-25 15:31:14 +0330

    2
    in this code i add three different number and i asked to compare them and print the biggest number!

---
+++ PY002.py
@@ -0,0 +1,8 @@
+A, B, C =("a", "b", "c")
+A, B, C = (54, 63, 97)
+if A>B and A>C:
+    print("A IS THE BIGGEST NUM")
+elif B>A and B>C:
+    print("B IS THE BIGGEST NUM")    
+elif C>B and C>A:
+    print("C IS THE BIGGEST NUM")    
