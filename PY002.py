40dc486de388126ec88ba056c7624a8e5a055636
TahaAbszd <146336406+TahaAbszd@users.noreply.github.com>
2024-01-25 21:15:16 +0330

    Delete PY002.py

--- PY002.py
+++
@@ -1,8 +0,0 @@
-A, B, C =("a", "b", "c")
-A, B, C = (54, 63, 97)
-if A>B and A>C:
-    print("A IS THE BIGGEST NUM")
-elif B>A and B>C:
-    print("B IS THE BIGGEST NUM")    
-elif C>B and C>A:
-    print("C IS THE BIGGEST NUM")    
